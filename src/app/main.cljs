877455d697d10588c000dcf7572ae8eed5893fa8
jiyinyiyong <jiyinyiyong@gmail.com>
2019-06-24 01:37:56 +0800

    prepare for rsync

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -3,7 +3,11 @@
   (:require [cumulo-util.file :refer [sh!]])
   (:require-macros [clojure.core.strint :refer [<<]]))
 
-(defn upload! [] (println "Upload file."))
+(defn upload! []
+  (println "Upload file.")
+  (let [cdn-folder "cdn", files "index.html,manifest.json", upload-folder "code"]
+    (sh! (<< "rsync -avr --progress dist/* ~{cdn-folder}"))
+    (sh! (<< "rsync -avr --progress dist/{~{files}} ~{upload-folder}"))))
 
 (defn main! [] (println "Started.") (upload!))
 
